49c40c58c4bad82d36b58fc20499aebca2510199
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2023-07-27 12:30:40 +0300

    feat(*): add base functionality

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { FullOptions, MapperResult, Methods, Options, OptionsWithKey, OptionsWithMethod } from "./types";
+
+/**
+ * Strongly typed utility function for mapping values
+ *
+ * @param map Mapping object
+ * @param key Key of the object to get a value from
+ * @param options Mapping options
+ * @returns Returns the value of the giving key or undefined
+ * @example
+ *
+ * getMappedValue({ one: 1 }, 'one'); // 1
+ *
+ * getMappedValue({ one: 1 }, 'two'); // undefined
+ *
+ * getMappedValue({ one: 1 }, 'ONE', { normalizingMethod: 'toLowerCase' }); // 1
+ *
+ * getMappedValue({ one: 1 }, 'ONE', { defaultKey: 'one' }); // 1
+ *
+ */
+export function getMappedValue<O extends Record<string, unknown>, T extends string>(map: O, key: T): MapperResult<O, T, undefined, undefined>;
+export function getMappedValue<O extends Record<string, unknown>, T extends string, D extends keyof O>(map: O, key: T, options: OptionsWithKey<D>): MapperResult<O, T, D, undefined>;
+export function getMappedValue<O extends Record<string, unknown>, T extends string, M extends Methods>(map: O, key: T, options: OptionsWithMethod<M>): MapperResult<O, T, undefined, M>;
+export function getMappedValue<O extends Record<string, unknown>, T extends string, D extends keyof O, M extends Methods>(map: O, key: T, options: FullOptions<D, M>): MapperResult<O, T, D, M>;
+export function getMappedValue<O extends Record<string, unknown>, T extends string, D extends keyof O, M extends Methods>(map: O, key: T, options: Options<D, M> = {}) {
+    const { defaultKey, normalizingMethod } = options;
+
+    const formattedKey = normalizingMethod ? key[normalizingMethod]() : key;
+    const defaultValue = defaultKey && map[defaultKey];
+
+    return map[formattedKey] || defaultValue;
+}
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+export type Methods = "toLowerCase" | "toUpperCase";
+export type Options<D, M> = { defaultKey?: D; normalizingMethod?: M };
+export type OptionsWithKey<D> = { defaultKey: D };
+export type OptionsWithMethod<M> = { normalizingMethod: M };
+export type FullOptions<D, M> = { defaultKey: D; normalizingMethod: M };
+
+type Nullable<T> = T | undefined;
+
+type AllValues<O extends Record<string, unknown>> = O[keyof O] | undefined;
+
+type DefaultValue<O extends Record<string, unknown>, D extends Nullable<keyof O>> = D extends undefined
+    ? undefined
+    : O[NonNullable<D>];
+
+type DefaultMethod<O extends Record<string, unknown>, T extends string, D extends Nullable<keyof O>> = {
+    [K in T]: K extends `${Exclude<keyof O, symbol>}` ? O[K] : DefaultValue<O, D>;
+}[T];
+
+type MappedMethod<
+    O extends Record<string, unknown>,
+    T extends string,
+    D extends Nullable<keyof O>,
+    M extends Methods
+> = {
+    toLowerCase: {
+        [K in T]: Lowercase<K> extends `${Exclude<keyof O, symbol>}` ? O[Lowercase<K>] : DefaultValue<O, D>;
+    }[T];
+    toUpperCase: {
+        [K in T]: Uppercase<K> extends `${Exclude<keyof O, symbol>}` ? O[Uppercase<K>] : DefaultValue<O, D>;
+    }[T];
+}[M];
+
+type MappedValue<
+    O extends Record<string, unknown>,
+    T extends string,
+    D extends Nullable<keyof O>,
+    M extends Nullable<Methods>
+> = M extends undefined ? DefaultMethod<O, T, D> : MappedMethod<O, T, D, NonNullable<M>>;
+
+export type MapperResult<
+    O extends Record<string, unknown>,
+    T extends string,
+    D extends Nullable<keyof O>,
+    M extends Nullable<Methods>
+> = string extends T ? AllValues<O> : MappedValue<O, T, D, M>;
